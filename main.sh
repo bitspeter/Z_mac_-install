43cc08340fd6aca8adc60caebb071325c1fc15ad
bitspeter <peter@bostonitservices.com>
2016-04-22 11:40:20 -0400

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#install apps
+Caskroom/cask/google-chrome
+Caskroom/cask/microsoft-office 
