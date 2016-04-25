e1d543a2c42aa5adebc602e819a882f9b7bdc1ad
bitspeter <peter@bostonitservices.com>
2016-04-25 09:19:35 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,5 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #install apps
-Caskroom/cask/google-chrome
-Caskroom/cask/microsoft-office 
+brew install Caskroom/cask/google-chrome
+brew install Caskroom/cask/microsoft-office 
+brew install Caskroom/cask/adobe-reader
+brew install Caskroom/cask/google-drive
